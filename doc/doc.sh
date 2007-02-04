fa9b37cc7918c5135a6e6fba163065d44751ba6b
menzies.tim <menzies.tim@dec426c2-f126-0410-bdfb-514567a8d945>
2007-02-04 17:19:26 +0000

    copyright notices added to doc.sh example and sandbox.emf

--- doc/doc.sh
+++ doc/doc.sh
@@ -64,6 +64,21 @@ cat<<-'EOF'
 	<a list of related man pages>
 	.SH BUGS
 	<known bugs if any>
+	.SH COPYRIGHT
+	Copyright <year> by <Your Name>
+	.P
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of version 2 of the GNU General Public License as 
+	published by the Free Software Foundation.
+	.P
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	.P
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 	EOF
 }
 main $1
